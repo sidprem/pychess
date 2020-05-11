e36abe488c909d21625089eb0d62ff41bba65ab9
sidprem <sidpr94@gmail.com>
2020-05-11 00:35:54 -0400

    legal moves generator and apply move created

--- Movegen.py
+++ Movegen.py
@@ -10,17 +10,17 @@ from Bitboard import shiftBitBoard, BB_SQUARES, \
     BB_H_FILE, BB_RANK_1, BB_RANK_2, BB_RANK_3, BB_RANK_4, BB_RANK_5, BB_RANK_6, BB_F1, BB_G1, \
     BB_RANK_7, BB_RANK_8, BB_EMPTY, BB_ALL_SQUARES, BB_NOT_A_FILE, BB_NOT_B_FILE, BB_B8, BB_C8, BB_D8, \
     BB_NOT_H_FILE, BB_NOT_G_FILE, BB_SQUARES, prettyPrintBitBoard, iterBits, pop_lsb, pop_count, BB_F8, BB_G8
-    
+
 from Utils import WHITE, BLACK, N, S, E, W, PAWN, KNIGHT, KING, BISHOP, ROOK, QUEEN, \
     moveFlag, NORMAL, ENPASSANT, KING_CASTLE, QUEEN_CASTLE, W_OO, W_OOO, B_OO, B_OOO
 
 from MoveTables import attacksFrom, attackersTo, betweenBB
 
-from itertools import chain 
+from itertools import chain
 
 #0-5 from
 #6-11 to
-#12-16 Normal, enpassant, king castle, queen castle, knight promo, bishop promo, rook promo, queen promo 
+#12-16 Normal, enpassant, king castle, queen castle, knight promo, bishop promo, rook promo, queen promo
 
 #create move
 def createMove(orig,des,flag):
@@ -37,10 +37,10 @@ def getFlag(move):
 
 #generate evasions
 def generateEvasionMoves(pos):
-    
+
     us = pos.color
     notFriends = ~pos.us & BB_ALL_SQUARES
-    
+
     #double check
     if pop_count(pos.checkers) > 1:
         #king moves out of check
@@ -48,9 +48,9 @@ def generateEvasionMoves(pos):
         checkerAttacks = 0
         for cord in iterBits(pos.checkers):
             checkerAttacks |= attacksFrom(pos.pieceBoard[cord],cord,blocker=0)
-        
+
         notCheckAttacks = ~checkerAttacks & BB_ALL_SQUARES
-        
+
         for bits in iterBits(usKing):
             for m in iterBits(attacksFrom(KING,bits) & notFriends & notCheckAttacks):
                 yield createMove(bits,m,0)
@@ -61,16 +61,16 @@ def generateEvasionMoves(pos):
         checkerAttacks = 0
         cord = pop_lsb(pos.checkers)
         checkerAttacks = attacksFrom(pos.pieceBoard[cord],cord,blocker=0)
-        
+
         notCheckAttacks = ~checkerAttacks & BB_ALL_SQUARES
-        
+
         kingSq = pop_lsb(usKing)
         for m in iterBits(attacksFrom(KING,kingSq) & notFriends & notCheckAttacks):
             yield createMove(kingSq,m,0)
-                
+
         #capture checker
         checkerAttackers = attackersTo(pos.board[pos.color],pos.blocker,cord,pos.color)
-        
+
         promoteRank = BB_RANK_7 if us == WHITE else BB_RANK_2
         for bits in iterBits(checkerAttackers):
             if pos.pieceBoard[bits] == PAWN:
@@ -80,55 +80,54 @@ def generateEvasionMoves(pos):
                 else:
                     yield createMove(bits,cord,0)
             else:
-                yield createMove(bits,cord,0)   
-        
+                yield createMove(bits,cord,0)
+
         #check if en-passant capture will remove checker
         direction = N if us == WHITE else S
         if (pos.enpassant == cord + direction) & (pos.pieceBoard[cord] == PAWN):
             pawnsAtEp = attackersTo(pos.board[us],pos.blocker,pos.enpassant,us)
             for bits in iterBits(pawnsAtEp):
                 yield createMove(bits,pos.enpassant,ENPASSANT)
-                
+
         #block check
         between = betweenBB(cord,kingSq)
-        prettyPrintBitBoard(between)
         for bb in pos.board[us][KNIGHT:QUEEN]:
             for sq in iterBits(bb):
-                piece = pos.pieceBoard[sq]  
+                piece = pos.pieceBoard[sq]
                 if piece != KING:
                     attack = attacksFrom(piece,sq,color=us,blocker=pos.blocker)
                     for m in iterBits(attack & between):
-                        yield createMove(sq,m,0)     
-        
-        #pawns set-wise 
+                        yield createMove(sq,m,0)
+
+        #pawns set-wise
         usPawns = pos.board[us][PAWN]
-    
+
         emptySquares = ~(pos.blocker) & BB_ALL_SQUARES
-        
+
         pawnsToPromote = (usPawns & BB_RANK_7) if us == WHITE else (usPawns & BB_RANK_2)
-        
+
         pawnsNotMoved = (usPawns & BB_RANK_2) if us == WHITE else (usPawns & BB_RANK_7)
-    
+
         pawnsNotPromote = usPawns & (~pawnsToPromote & BB_ALL_SQUARES)
-        
+
         pawnNormalMoves = shiftBitBoard(pawnsNotPromote,direction) & emptySquares & between
-        
+
         for bits in iterBits(pawnNormalMoves):
             yield createMove(bits - direction, bits, NORMAL)
-        
+
         pawnNotMovedShift = shiftBitBoard(pawnsNotMoved,direction) & emptySquares
         pawnDoubleMoves = shiftBitBoard(pawnNotMovedShift,direction) & emptySquares & between
-        
+
         for bits in iterBits(pawnDoubleMoves):
             yield createMove(bits - 2*direction, bits, NORMAL)
-        
+
         #quiet pawn promotions bb
         pawnPromotions = shiftBitBoard(pawnsToPromote,direction) & emptySquares & between
-        
+
         for bits in iterBits(pawnPromotions):
             for p in moveFlag[4:]:
                 yield createMove(bits - direction, bits, p)
-                
+
 #generate psuedo moves
 def generatePseudoLegalMoves(pos):
     pawnMoves = generatePawnMoves(pos)
@@ -137,171 +136,209 @@ def generatePseudoLegalMoves(pos):
     bishopMoves = generateBishopMoves(pos)
     rookMoves = generateRookMoves(pos)
     queenMoves = generateQueenMoves(pos)
-    
+
     return chain(pawnMoves,knightMoves,kingMoves,bishopMoves,rookMoves,queenMoves)
 
 #pseudo legal pawn moves pawn moves
 def generatePawnMoves(pos):
-    
+
     #get pawns
     us = pos.color
-    
+
     direction = N if us == WHITE else S
-    
+
     enemies = pos.them
-    
+
     usPawns = pos.board[us][PAWN]
-    
+
     emptySquares = ~(pos.blocker) & BB_ALL_SQUARES
-    
+
     pawnsToPromote = (usPawns & BB_RANK_7) if us == WHITE else (usPawns & BB_RANK_2)
-    
+
     pawnsNotMoved = (usPawns & BB_RANK_2) if us == WHITE else (usPawns & BB_RANK_7)
 
     pawnsNotPromote = usPawns & (~pawnsToPromote & BB_ALL_SQUARES)
-    
+
     pawnNormalMoves = shiftBitBoard(pawnsNotPromote,direction) & emptySquares
-    
+
     for bits in iterBits(pawnNormalMoves):
         yield createMove(bits - direction, bits, NORMAL)
-    
+
     pawnNotMovedShift = shiftBitBoard(pawnsNotMoved,direction) & emptySquares
     pawnDoubleMoves = shiftBitBoard(pawnNotMovedShift,direction) & emptySquares
-    
+
     for bits in iterBits(pawnDoubleMoves):
         yield createMove(bits - 2*direction, bits, NORMAL)
-    
+
     #quiet pawn promotions bb
     pawnPromotions = shiftBitBoard(pawnsToPromote,direction) & emptySquares
-    
+
     for bits in iterBits(pawnPromotions):
         for p in moveFlag[4:]:
             yield createMove(bits - direction, bits, p)
-    
+
     #capture pawn moves
     pawnCapturesNE = shiftBitBoard(pawnsNotPromote,direction + E) & enemies
     for bits in iterBits(pawnCapturesNE):
         yield createMove(bits - (direction + E), bits, NORMAL)
-        
+
     pawnCapturesNW = shiftBitBoard(pawnsNotPromote,direction + W) & enemies
     for bits in iterBits(pawnCapturesNW):
         yield createMove(bits - (direction + W), bits, NORMAL)
-        
+
     #capture pawn promotions
     pawnCapturePromoNE = shiftBitBoard(pawnsToPromote,direction + E) & enemies
     for bits in iterBits(pawnCapturePromoNE):
         for p in moveFlag[4:]:
             yield createMove(bits - (direction + E), bits, p)
-            
+
     pawnCapturePromoNW = shiftBitBoard(pawnsToPromote,direction + W) & enemies
     for bits in iterBits(pawnCapturePromoNW):
         for p in moveFlag[4:]:
             yield createMove(bits - (direction + W), bits, p)
-    
+
     if(pos.enpassant != -1):
         pawnsAtEp = attackersTo(pos.board[us],pos.blocker,pos.enpassant,us)
         for bits in iterBits(pawnsAtEp):
             yield createMove(bits,pos.enpassant,ENPASSANT)
-            
+
 def generateKingMoves(pos):
-    
+
     us = pos.color
-    
+
     usKing = pos.board[us][KING]
-    
+
     emptySquares = ~(pos.blocker) & BB_ALL_SQUARES
-    
+
     notFriends = ~pos.us & BB_ALL_SQUARES
-    
+
     for bits in iterBits(usKing):
         for m in iterBits(attacksFrom(KING,bits) & notFriends):
             yield createMove(bits,m,0)
-    
+
     if us == WHITE:
         if pos.castles & W_OO:
-            stepOvers = BB_F1 | BB_G1
-            if(stepOvers & emptySquares):
+            wooStepOvers = betweenBB(4,7)
+            if (wooStepOvers & emptySquares) == wooStepOvers:
                 yield createMove(4,6,KING_CASTLE)
-        elif pos.castles & W_OOO:
-            stepOvers = BB_B1 | BB_C1 | BB_D1
-            if(stepOvers & emptySquares):
+        if pos.castles & W_OOO:
+            woooStepOvers = betweenBB(0,4)
+            if (woooStepOvers & emptySquares) == woooStepOvers:
                 yield createMove(4,2,QUEEN_CASTLE)
     elif us == BLACK:
         if pos.castles & B_OO:
-            stepOvers = BB_F8 | BB_G8
-            if(stepOvers & emptySquares):
+            booStepOvers = betweenBB(60,63)
+            if (booStepOvers & emptySquares) == booStepOvers:
                 yield createMove(60,62,KING_CASTLE)
-        elif pos.castles & B_OOO:
-            stepOvers = BB_B8 | BB_C8 | BB_D8
-            if(stepOvers & emptySquares):
-                yield createMove(60,58,QUEEN_CASTLE)        
-    
-    
+        if pos.castles & B_OOO:
+            boooStepOvers = betweenBB(56,60)
+            if (boooStepOvers & emptySquares) == boooStepOvers:
+                yield createMove(60,58,QUEEN_CASTLE)
+
+
 def generateKnightMoves(pos):
 
     # get color
     us = pos.color
-    
+
     usKnights = pos.board[us][KNIGHT]
-        
+
     notFriends = ~pos.us & BB_ALL_SQUARES
-    
+
     for bits in iterBits(usKnights):
         for m in iterBits(attacksFrom(KNIGHT,bits) & notFriends):
             yield createMove(bits,m,0)
 
 def generateBishopMoves(pos):
-    
+
     us = pos.color
-    
+
     blocker = pos.blocker
 
     usBishops = pos.board[us][BISHOP]
-    
+
     notFriends = ~pos.us & BB_ALL_SQUARES
-    
+
     for sq in iterBits(usBishops):
 
         attack = attacksFrom(BISHOP,sq,blocker=blocker) & notFriends
-        
+
         for m in iterBits(attack):
             yield createMove(sq,m,0)
 
 def generateRookMoves(pos):
-    
+
     us = pos.color
-        
+
     blocker = pos.blocker
-    
+
     usRooks = pos.board[us][ROOK]
-    
+
     notFriends = ~pos.us & BB_ALL_SQUARES
-    
+
     for sq in iterBits(usRooks):
-        
+
         attack = attacksFrom(ROOK,sq,blocker=blocker) & notFriends
-        
+
         for m in iterBits(attack):
-            yield createMove(sq,m,0) 
-            
+            yield createMove(sq,m,0)
+
 def generateQueenMoves(pos):
-    
+
     us = pos.color
-    
+
     blocker = pos.blocker
-    
+
     usQueens = pos.board[us][QUEEN]
-    
+
     notFriends = ~pos.us & BB_ALL_SQUARES
-    
+
     for sq in iterBits(usQueens):
         attack = (attacksFrom(BISHOP,sq,blocker=blocker) | attacksFrom(ROOK,sq,blocker=blocker)) & notFriends
         for m in iterBits(attack):
-            yield createMove(sq,m,0) 
+            yield createMove(sq,m,0)
 
 def generateLegalMoves(pos):
-    
-    moveList = generateEvasionMoves(pos) if pos.checkers > 0 else generatePseudoLegalMoves(pos)
-    
-    return moveList
+
+    moveList = generateEvasionMoves(pos) if pos.isInCheck() else generatePseudoLegalMoves(pos)
+
+    absolutelyPinned = pos.pinned
+
+    for move in moveList:
         
+        orig = getOrig(move)
+        des = getDes(move)
+        flag = getFlag(move)
+            
+        if flag == ENPASSANT:
+            newUsPieces = pos.us ^ BB_SQUARES[orig] ^ BB_SQUARES[des]
+            direction = N if pos.color == WHITE else S
+            newThemPieces = pos.them ^ BB_SQUARES[des - direction]
+            newBoard = pos.board
+            newBoard[pos.opColor][0] =newBoard[pos.opColor][0] ^ BB_SQUARES[des - direction]
+            checkers = attackersTo(newBoard[pos.opColor],newUsPieces | newThemPieces,pos.king,pos.opColor)
+            if checkers == 0:
+                yield move
+        elif flag == KING_CASTLE:
+            stepOver = betweenBB(4,7) if  pos.color == WHITE else betweenBB(60,63)
+            squareAttacked = 0
+            for sq in iterBits(stepOver):
+                squareAttacked |= attackersTo(pos.board[pos.opColor],pos.blocker,sq,pos.opColor)
+            if squareAttacked == 0:
+                yield move
+        elif flag == QUEEN_CASTLE:
+            stepOver = betweenBB(0,4) if  pos.color == WHITE else betweenBB(56,60)
+            squareAttacked = 0
+            for sq in iterBits(stepOver):
+                squareAttacked |= attackersTo(pos.board[pos.opColor],pos.blocker,sq,pos.opColor)
+            if squareAttacked == 0:
+                yield move
+
+        if pos.pieceBoard[orig] == KING:
+            if flag == NORMAL:
+                squareAttacked = attackersTo(pos.board[pos.opColor],pos.blocker,des,pos.opColor)
+                if ~(squareAttacked > 0) & 1:
+                    yield move
+        elif (~absolutelyPinned & BB_ALL_SQUARES) & BB_SQUARES[orig]:
+            yield move
+
--- Position.py
+++ Position.py
@@ -8,18 +8,20 @@ Created on Fri Apr 17 12:26:34 2020
 from Bitboard import setBits, clearBits
 
 from Utils import PAWN, KNIGHT, KING, BISHOP, ROOK, QUEEN, WHITE, BLACK, pieceStr, colorStr, W_OO, W_OOO, \
-    B_OO, B_OOO, algebraic
+    B_OO, B_OOO, algebraic, moveFlag, NORMAL, ENPASSANT, KING_CASTLE, QUEEN_CASTLE, KNIGHT_PROMO, BISHOP_PROMO, \
+    ROOK_PROMO,QUEEN_PROMO, N, S
 
 from MoveTables import attackersTo, blockers
 
+from Movegen import getOrig, getDes, getFlag
+
 class Position:
     def __init__(self):
-        self.board = ([0]*6,[0]*6) #bitboard of each color for each piece
+        self.board = [[0]*6,[0]*6] #bitboard of each color for each piece
         self.pieceBoard = [6]*64 #board of pieceType for each cord
         self.enpassant = -1 #en passant target square cord
         self.king = 0 #king square cord
         self.castles = 0 #castle indicator W_OO | W_OOO | B_OO | B_OOO
-        #TODO: self.movehistory = []
         #TODO: self.repetition = 0
         self.color = 0 #color of mover
         self.opColor = 0 #color of Op
@@ -28,20 +30,33 @@ class Position:
         self.blocker = 0 #blockers on the board
         self.pinned = 0 #pieces absolutely pinned
         self.checkers = 0 #bitboard of all pieces attacking the mover king
-        
+        self.ply = 0
+
+        self.history = {
+            "enpassant" : [],
+            "castles" : [],
+            "moves" : [],
+            "captured" : [],
+            "color" : [],
+            "checkers" : [],
+            "us" : [],
+            "them" : [],
+            "blocker" : [],
+            "pinned" : []
+            }
+
     def posFromFEN(self,fen):
         state = fen.split()
-        
+
         pieces = state[0]
         mover = state[1]
         castles = state[2]
         ep = state[3]
-        #TODO: full_move = state[4]
-        #TODO: half_move = state[5]
-        
+        half_move = state[5]
+
         self.color = colorStr[mover]
         self.opColor = ~self.color & 1
-        
+
         for r,rank in enumerate(pieces.split("/")):
             cord = (7-r)*8
             for char in rank:
@@ -52,8 +67,8 @@ class Position:
                     color = WHITE if char.isupper() else BLACK
                     self.setPiece(piece,color,cord)
                     cord+=1
-                    
-        self.blocker = self.us | self.them      
+
+        self.blocker = self.us | self.them
         
         for char in castles:
             if char == "K":
@@ -64,17 +79,23 @@ class Position:
                 self.castles = self.castles | B_OO
             elif char == "q":
                 self.castles = self.castles | B_OOO
-                
+
         if ep != "-":
             self.enpassant = algebraic.index(ep)
         else:
             self.enpassant = -1
-        
+            
         self.checkers = attackersTo(self.board[self.opColor],self.blocker,self.king,self.opColor)
         self.pinned = blockers(self.board[self.opColor],self.blocker,self.king)
         
-        #TODO: Half Move & Full Move counter
-    
+        self.history["blocker"].append(self.blocker)
+        self.history["castles"].append(self.castles)
+        self.history["color"].append(self.color)
+        self.history["enpassant"].append(self.enpassant)
+        self.history["checkers"].append(self.checkers)
+        self.history["pinned"].append(self.pinned)
+        self.ply = half_move
+
     def setPiece(self,piece,color,cord):
         self.board[color][piece] = setBits(self.board[color][piece],cord)
         self.pieceBoard[cord] = piece
@@ -83,11 +104,100 @@ class Position:
            if(piece == KING):
                self.king = cord
         else:
-           self.them = setBits(self.them,cord) 
-           
+           self.them = setBits(self.them,cord)
+
     def clearPiece(self,piece,color,cord):
         self.board[color][piece] = clearBits(self.board[color][piece],cord)
+        if self.color == color:
+           self.us = clearBits(self.us,cord)
+        else:
+           self.them = clearBits(self.them,cord)
         self.pieceBoard[cord] = -1
-        
+
     def isInCheck(self):
-        return 1 if self.checkers else 0 
+        return 1 if self.checkers else 0
+
+    def applyMove(self,move):
+
+        self.history["moves"].append(move)
+        orig = getOrig(move)
+        des = getDes(move)
+        flag = getFlag(move)
+
+        mPiece = self.pieceBoard[orig]
+        tPiece = self.pieceBoard[des]
+        direction = N if self.color == WHITE else S
+
+        #enpassant
+        if flag == ENPASSANT:
+            self.clearPiece(mPiece,self.color,orig)
+            self.clearPiece(tPiece,self.opColor,des - direction)
+            self.setPiece(mPiece,self.color,des)
+            self.history["captured"].append(tPiece)
+        elif flag == KING_CASTLE:
+            rookInitial = 7 if self.color == WHITE else 63
+            rookPiece = self.pieceBoard[rookInitial]
+            self.clearPiece(mPiece,self.color,orig)
+            self.clearPiece(rookPiece,self.color,rookInitial)
+            self.setPiece(mPiece,self.color,des)
+            self.setPiece(rookPiece,self.color,des-1)
+            if self.color == WHITE:
+                self.castles = self.castles ^ W_OO ^ W_OOO
+            else:
+                self.castles = self.castles ^ B_OO ^ B_OOO    
+        elif flag == QUEEN_CASTLE:
+            rookInitial = 0 if self.color == WHITE else 56
+            rookPiece = self.pieceBoard[rookInitial]
+            self.clearPiece(mPiece,self.color,orig)
+            self.clearPiece(rookPiece,self.color,rookInitial)
+            self.setPiece(mPiece,self.color,des)
+            self.setPiece(rookPiece,self.color,des+ 1)
+            if self.color == WHITE:
+                self.castles = self.castles ^ W_OOO ^ W_OO
+            else:
+                self.castles = self.castles ^ B_OOO ^ B_OO
+        elif flag == KNIGHT_PROMO:
+            self.clearPiece(mPiece,self.color,orig)
+            self.setPiece(KNIGHT,self.color,des)
+        elif flag == BISHOP_PROMO:
+            self.clearPiece(mPiece,self.color,orig)
+            self.setPiece(BISHOP,self.color,des)
+        elif flag == ROOK_PROMO:
+            self.clearPiece(mPiece,self.color,orig)
+            self.setPiece(ROOK,self.color,des)
+        elif flag == QUEEN_PROMO:
+            self.clearPiece(mPiece,self.color,orig)
+            self.setPiece(QUEEN,self.color,des)
+        elif mPiece == PAWN:
+            self.clearPiece(mPiece,self.color,orig)
+            self.setPiece(mPiece,self.color,des)
+            if(des - orig == 16):
+                self.enpassant = des - direction
+        else:
+            self.clearPiece(mPiece,self.color,orig)
+            self.setPiece(mPiece,self.color,des)
+            if (mPiece == KING) | (mPiece == ROOK):
+                if self.color == WHITE:
+                    self.castles = self.castles ^ W_OOO ^ W_OO
+                else:
+                    self.castles = self.castles ^ B_OOO ^ B_OO 
+                    
+        self.history["color"].append(self.color)
+        self.color = self.opColor
+        self.opColor = ~self.color & 1
+        
+        self.enpassant = -1
+        self.blocker = self.us | self.them
+        self.us = self.them
+        self.them = self.blocker ^ self.us
+        self.checkers = attackersTo(self.board[self.opColor],self.blocker,self.king,self.opColor)
+        self.pinned = blockers(self.board[self.opColor],self.blocker,self.king)
+        
+        self.history["captured"].append(tPiece)
+        self.history["blocker"].append(self.blocker)
+        self.history["castles"].append(self.castles)
+        self.history["enpassant"].append(self.enpassant)
+        self.history["checkers"].append(self.checkers)
+        self.history["pinned"].append(self.pinned)
+        
+        self.ply += 1
